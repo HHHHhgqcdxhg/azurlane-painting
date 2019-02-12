82e5e2307fe93c6dd2c8c62adf7ec7df18fe1913
HHHHhg <2894700792@qq.com>
2019-02-12 22:29:55 +0800

    做个exe

---
+++ alpainting/alpainting/alpainting/alpainting.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <direct.h> 
+using namespace std;
+
+int main(int argc, char * argv[])
+{
+	string cwd = _getcwd(NULL, 0);
+	string cmd = "\\py37\\python.exe __main__.py";
+	cmd = cwd + cmd;
+	for (int i = 1; i < argc; i++)
+	{
+		cmd += " ";
+		cmd += argv[i];
+	}
+	const char *cmd_c_str = cmd.c_str();
+	cout << cmd_c_str << endl;
+	system(cmd_c_str);
+	//system("pause");
+	return 0;
+}
