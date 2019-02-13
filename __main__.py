0cc23aec6b6c367ec2c30c15f69a92acd57c3b76
HHHHhg <2894700792@qq.com>
2019-02-14 03:59:51 +0800

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,6 +1,5 @@
-if __name__ == '__main__':
+def getArgs():
     import argparse
-
     parser = argparse.ArgumentParser(description='碧蓝航线绘图日记像素画生成工具')
     parser.add_argument('input', metavar='input', type=str, help='input file path')
     parser.add_argument('-o', '--output', type=str, help='output file path. image end by .png,video end by .avi')
@@ -14,7 +13,15 @@ if __name__ == '__main__':
                         help='extrace one frame in cutFrame(your input num) frames,mode video only', default=1)
     # parser.add_argument('-c', '--cutFrames', type=int, help='extrace one frame in cutFrame(your input num) frames,mode video only',default=1)
     args = parser.parse_args()
+    return args
+
+
+def mainFunc(args):
     import os
+
+    if not os.path.exists(args.input):
+        print("error: input file not found!")
+        return
     if args.mode == "image":
         print(f"start draw image {args.input}......")
         import paint
@@ -34,7 +41,7 @@ if __name__ == '__main__':
             oPaths = os.path.split(absInputPath)
             rawFileName = oPaths[1]
             outFileName = f"{rawFileName.split('.')[0]}-{int(time.time())}.png"
-            oPath = os.path.join(oPaths[0],outFileName)
+            oPath = os.path.join(oPaths[0], outFileName)
             # oPath = f"azurlanePainting-{int(time.time())}.png"
         o.save(oPath, "PNG")
         print(f"image drawn completed -> {oPath}")
@@ -55,5 +62,11 @@ if __name__ == '__main__':
             #
             # oPath = f"azurlanePainting-{int(time.time())}.avi"
         print(f"start handdle video {args.input}......")
-        analyseVideo.makeVideo(inputVideoPath=args.input, output=oPath,cutFrames=args.cutFrames, w=args.width, h=args.height,blur=args.bold)
+        analyseVideo.makeVideo(inputVideoPath=args.input, output=oPath, cutFrames=args.cutFrames, w=args.width,
+                               h=args.height, blur=args.bold)
         print(f"video handdled completed -> {oPath}")
+
+if __name__ == '__main__':
+    args = getArgs()
+    mainFunc(args)
+
