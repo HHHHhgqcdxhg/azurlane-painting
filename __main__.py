4d80aa9697116c1aff0f69fe3e996887d607761c
HHHHhg <2894700792@qq.com>
2019-02-14 03:39:35 +0800

    输出文件默认位置更改

--- __main__.py
+++ __main__.py
@@ -14,23 +14,28 @@ if __name__ == '__main__':
                         help='extrace one frame in cutFrame(your input num) frames,mode video only', default=1)
     # parser.add_argument('-c', '--cutFrames', type=int, help='extrace one frame in cutFrame(your input num) frames,mode video only',default=1)
     args = parser.parse_args()
-
+    import os
     if args.mode == "image":
+        print(f"start draw image {args.input}......")
         import paint
-
         # if args.x is None:
         #     args.x = 1
         # if args.y is None:
         #     args.y = 1
-        print(f"start draw image {args.input}......")
+
         o = paint.drawN(imgPath=args.input, w=args.width, h=args.height, blur=args.bold)
+        absInputPath = os.path.abspath(args.input)
         if not args.output is None:
             oPath = args.output
 
         else:
             import time
-
-            oPath = f"azurlanePainting-{int(time.time())}.png"
+            absInputPath = os.path.abspath(args.input)
+            oPaths = os.path.split(absInputPath)
+            rawFileName = oPaths[1]
+            outFileName = f"{rawFileName.split('.')[0]}-{int(time.time())}.png"
+            oPath = os.path.join(oPaths[0],outFileName)
+            # oPath = f"azurlanePainting-{int(time.time())}.png"
         o.save(oPath, "PNG")
         print(f"image drawn completed -> {oPath}")
     elif args.mode == "video":
@@ -42,7 +47,13 @@ if __name__ == '__main__':
         else:
             import time
 
-            oPath = f"azurlanePainting-{int(time.time())}.avi"
+            absInputPath = os.path.abspath(args.input)
+            oPaths = os.path.split(absInputPath)
+            outFileName = f"{oPaths[1].split('.')[0]}-{int(time.time())}.avi"
+            oPath = os.path.join(oPaths[0], outFileName)
+            # import time
+            #
+            # oPath = f"azurlanePainting-{int(time.time())}.avi"
         print(f"start handdle video {args.input}......")
         analyseVideo.makeVideo(inputVideoPath=args.input, output=oPath,cutFrames=args.cutFrames, w=args.width, h=args.height,blur=args.bold)
         print(f"video handdled completed -> {oPath}")
